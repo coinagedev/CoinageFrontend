23e26f1ec49cc746b211933123cb0d537889e02f
mshahed <mshahed@catalyte.io>
2022-04-16 22:47:38 -0400

    header_rf

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,15 @@
 import ParticlesBg from "particles-bg";
+import coinage from "../../src/coinage.jpg"
 
 export const Header = (props) => {
+  // var coinage = require('../../src/coinage.jpg')
   return (
     <header id='header'>
       <div className='intro'>
-        <ParticlesBg type="circle" bg={{zIndex: 0, position:"absolute", top:0}} />
+        <ParticlesBg type="thick" bg={{zIndex: 0, position:"absolute", top:0}} />
+        {/* <img src={coinage} width={1200} height={600} alt='image not found' /> */}
         <div className='overlay'>
           <div className='container'>
-          
             <div className='row'>
               <div className='col-md-8 col-md-offset-2 intro-text'>
                 <h1>
